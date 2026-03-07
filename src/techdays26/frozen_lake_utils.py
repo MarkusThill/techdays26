d665fbcd23811d697fe57071b4d2c09bdca50a65
MarkusThill <8896197+MarkusThill@users.noreply.github.com>
2026-03-07 20:14:56 +0100

    chore: added lab1 jupyter notebook

---
+++ src/techdays26/frozen_lake_utils.py
@@ -0,0 +1,24 @@
+import pygame 
+
+def get_action_from_keyboard(key_to_action):
+    """
+    Waits for keyboard input and returns the corresponding action.
+    Returns None if the window should be closed.
+    """
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return None
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return None
+                if event.key in key_to_action:
+                    return key_to_action[event.key]
+        pygame.time.wait(30)
+
+key_to_action = {
+        pygame.K_LEFT: 0,
+        pygame.K_DOWN: 1,
+        pygame.K_RIGHT: 2,
+        pygame.K_UP: 3,
+    }
